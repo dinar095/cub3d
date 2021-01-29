65b41ce29bacbd01d60f43f863d6d2281741305e
Desausag <dinar095@hotmail.com>
2021-01-29 14:47:16 +0300

    Change Cmake File

--- main.c
+++ main.c
@@ -24,22 +24,22 @@ int main(int argc, char **argv)
 		printf("%s\n", *(textures.map + len));
 		len++;
 	}
-//	void *mlx = NULL;
-//	void *win = NULL;
-//	int x = 100;
-//	int y = 100;
-//
-//	mlx = mlx_init();
-//	win = mlx_new_window(mlx, 640, 480, "test");
-//	while (y++ < 200)
-//	{
-//		x = 100;
-//		while (x++ < 200)
-//		{
-//			mlx_pixel_put(mlx, win, x, y, 0xDC640A);
-//		}
-//	}
-//	mlx_loop(mlx);
-//	ft_putchar_fd('s', 1);
+	void *mlx = NULL;
+	void *win = NULL;
+	int x = 100;
+	int y = 100;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 640, 480, "test");
+	while (y++ < 200)
+	{
+		x = 100;
+		while (x++ < 200)
+		{
+			mlx_pixel_put(mlx, win, x, y, 0xDC640A);
+		}
+	}
+	mlx_loop(mlx);
+	ft_putchar_fd('s', 1);
 	return 0;
 }
